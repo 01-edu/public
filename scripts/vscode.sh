a9d104879db5de13477a329f0632f360114bbdb2
Xavier Petit <le@vieux.pro>
2019-07-03 12:24:10 +0100

    Add code command

--- scripts/vscode.sh
+++ scripts/vscode.sh
@@ -10,6 +10,13 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' > /etc/apt/sources.list.d/vscodium.list
 apt-get update && apt-get -y install codium
 
+cat <<EOF> /usr/local/bin/code
+#!/usr/bin/env bash
+
+/usr/share/codium/codium &>/dev/null &
+EOF
+chmod +x /usr/local/bin/code
+
 # Set-up all users
 for DIR in $(ls -1d /home/* 2>/dev/null || true)
 do
