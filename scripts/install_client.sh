ba64ff3c1a2ec0984213f082b50d1d4232307e97
Xavier Petit <32063953+xpetit@users.noreply.github.com>
2019-09-12 18:51:24 +0100

    Remove outdated kernels

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -28,6 +28,13 @@ apt-get update
 apt-get -y upgrade
 apt-get -y autoremove --purge
 
+# Remove outdated kernels
+old_kernels=$(ls -1 /boot/config-* | xargs -n1 basename | grep -v "$(uname -r)" | cut -d- -f2,3)
+
+for old_kernel in $old_kernels; do
+	dpkg -P $(dpkg-query -f '${binary:Package}\n' -W *"$old_kernel"*)
+done
+
 . bash_tweaks.sh
 . ssh.sh
 . firewall.sh
