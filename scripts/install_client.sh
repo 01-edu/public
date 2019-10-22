2a65f29616f0497faa76c18342c0266fc5c0d5da
Xavier Petit <32063953+xpetit@users.noreply.github.com>
2019-10-22 17:22:11 +0200

    Comment buggy code

--- scripts/install_client.sh
+++ scripts/install_client.sh
@@ -29,11 +29,11 @@ apt-get -y upgrade
 apt-get -y autoremove --purge
 
 # Remove outdated kernels
-old_kernels=$(ls -1 /boot/config-* | sed '$d' | xargs -n1 basename | cut -d- -f2,3)
+# old_kernels=$(ls -1 /boot/config-* | sed '$d' | xargs -n1 basename | cut -d- -f2,3)
 
-for old_kernel in $old_kernels; do
-	dpkg -P $(dpkg-query -f '${binary:Package}\n' -W *"$old_kernel"*)
-done
+# for old_kernel in $old_kernels; do
+# 	dpkg -P $(dpkg-query -f '${binary:Package}\n' -W *"$old_kernel"*)
+# done
 
 apt-get -yf install
 
