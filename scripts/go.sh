7f69e3009d1185bdbff1ff00af7830210e8cb2d7
Xavier Petit <32063953+xpetit@users.noreply.github.com>
2020-01-19 11:38:54 +0000

    Change golang installation method

--- scripts/go.sh
+++ scripts/go.sh
@@ -6,7 +6,10 @@ script_dir="$(cd -P "$(dirname "$BASH_SOURCE")" && pwd)"
 cd $script_dir
 . set.sh
 
-apt-get -y install golang
+wget https://dl.google.com/go/go1.13.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.13.6.linux-amd64.tar.gz
+rm go1.13.6.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
 
 # Set-up all users
 for dir in $(ls -1d /root /home/* 2>/dev/null ||:)
@@ -15,7 +18,7 @@ do
 	cat <<-'EOF'>> $dir/.bashrc
 	GOPATH=$HOME/go
 	PATH=$PATH:$GOPATH/bin
-	alias gobuild='CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w"'
+	alias gobuild='CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-s -w"'
 	EOF
 	echo 'GOPATH=$HOME/go' >> $dir/.profile
 
