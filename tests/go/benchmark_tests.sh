f28920156c811838f93186d4bb1289a11ce96050
Xavier Petit <32063953+xpetit@users.noreply.github.com>
2020-04-07 15:59:58 +0200

    Modify benchmark accordingly to other scripts modifications

--- tests/go/benchmark_tests.sh
+++ tests/go/benchmark_tests.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -euo pipefail
+set -o errexit
+set -o pipefail
 IFS='
 '
 
-cd -P "$(dirname "$BASH_SOURCE")"
+cd -P "$(dirname "$0")"
 
-rm -rf student
+./clean.sh
 cp -a solutions student
 go test -v -json|jq -c 'select(.Action == "pass") | {Test, Elapsed}' | jq -sr 'sort_by(.Elapsed) | .[-30:] | .[] | [.Elapsed, .Test] | @tsv'
