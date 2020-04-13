96a697fb448cf8dacb2b6e428d14805d962a59f3
Xavier Petit <32063953+xpetit@users.noreply.github.com>
2020-04-13 19:10:31 +0200

    Refactor displayfile solution

--- tests/go/displayfile_test.go
+++ tests/go/displayfile_test.go
@@ -1,21 +1,20 @@
 package student_test
 
 import (
+	"os"
 	"strings"
 	"testing"
 
-	solutions "./solutions"
 	"github.com/01-edu/z01"
 )
 
 func TestDisplayfile(t *testing.T) {
-	var table []string
 	pathFileName := "./student/displayfile/quest8.txt"
-
-	solutions.CheckFile(t, pathFileName)
-
-	table = append(table, "", pathFileName, "quest8.txt asdsada")
-
+	_, err := os.Stat(pathFileName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	table := []string{"", pathFileName, "quest8.txt asdsada"}
 	for _, s := range table {
 		z01.ChallengeMain(t, strings.Fields(s)...)
 	}
