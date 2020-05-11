460a40f075fe149ea5fdbe92e26ab10980f19016
Xavier Petit <32063953+xpetit@users.noreply.github.com>
2020-05-11 18:58:15 +0200

    Fusion scripts

--- sh/scripts/fx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install FX: command-line JSON processing tool (https://github.com/antonmedv/fx)
-
-script_dir="$(cd -P "$(dirname "$BASH_SOURCE")" && pwd)"
-cd $script_dir
-. set.sh
-
-npm install -g fx
--- sh/scripts/libreoffice.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install VSCode
-
-script_dir="$(cd -P "$(dirname "$BASH_SOURCE")" && pwd)"
-cd $script_dir
-. set.sh
-
-apt-get -y install libreoffice
