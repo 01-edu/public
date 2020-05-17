fa5e11f72885ff4694f2c3743e6ce3733cff472d
⁣ <>
2020-05-17 16:59:25 +0200

    Fix mistake

--- go/tests/boolean_test/boolean_correct/main.go
+++ go/tests/boolean_test/boolean_correct/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	if len(os.Args)-1%2 == 0 {
+	if len(os.Args[1:])%2 == 0 {
 		fmt.Println("I have an even number of arguments")
 	} else {
 		fmt.Println("I have an odd number of arguments")
