76b7fdcb6064a1c82e08cf923a25f9fd97615793
⁣ <>
2020-05-17 11:34:12 +0200

    Update exercise to make it work in dev env

--- sh/tests/solutions/myfamily.sh
+++ sh/tests/solutions/myfamily.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-curl -s https://$DOMAIN/assets/superhero/all.json | jq ".[] | select( .id == $HERO_ID)" | grep relatives | cut -d'"' -f4
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+f=""
+# --insecure flag to make it work with dev environment (self-signed certificate)
+test "$DOMAIN" = "localhost" && f="--insecure"
+
+curl $f --compressed --silent --show-error --max-time 10 https://$DOMAIN/assets/superhero/all.json |
+    jq ".[] | select(.id == $HERO_ID)" | grep relatives | cut -d'"' -f4
