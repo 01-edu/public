0084b3fdcf4002fbe9cd8a14ebb99a048a994391
⁣ <>
2020-05-29 20:59:53 +0200

    PrintNbrBase fixed

--- go/tests/base/base.go
+++ go/tests/base/base.go
@@ -1,8 +1,60 @@
+package base
+
 import (
 	"math/rand"
+	"strings"
 
 	"lib"
 )
+
+func uniqueChar(s string) bool {
+	m := map[rune]struct{}{}
+	for _, r := range s {
+		if _, ok := m[r]; ok {
+			return false
+		}
+		m[r] = struct{}{}
+	}
+	return true
+}
+
+func IsValid(base string) bool {
+	return len(base) >= 2 && !strings.ContainsAny(base, "+-") && uniqueChar(base)
+}
+
+func power(nbr int, pwr int) int {
+	if pwr == 0 {
+		return 1
+	}
+	if pwr == 1 {
+		return nbr
+	}
+	return nbr * power(nbr, pwr-1)
+}
+
+func Atoi(s string, base string) int {
+	var result int
+	var i int
+	sign := 1
+	lengthBase := len(base)
+	lengths := len(s)
+
+	if !IsValid(base) {
+		return 0
+	}
+	if s[i] == '-' {
+		sign = -1
+	}
+	if s[i] == '-' || s[i] == '+' {
+		i++
+	}
+	for i < len(s) {
+		result += strings.Index(base, string(s[i])) * power(lengthBase, lengths-1)
+		i++
+		lengths--
+	}
+	return result * sign
+}
 
 func Valid() string {
 	valid := []string{
@@ -59,7 +111,7 @@ func ConvertNbr(n int, base string) string {
 }
 
 func Convert(nbr, baseFrom, baseTo string) string {
-	resultIntermediary := AtoiBase(nbr, baseFrom)
+	resultIntermediary := Atoi(nbr, baseFrom)
 
 	resultFinal := ConvertNbr(resultIntermediary, baseTo)
 
