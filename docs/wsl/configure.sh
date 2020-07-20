3a1438784a945d08b31a703104a4d44cf8e93391
xpetit <32063953+xpetit@users.noreply.github.com>
2020-07-20 19:17:28 +0200

    Create configure.sh

---
+++ docs/wsl/configure.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS='
+'
+
+echo Installing common tools
+sudo apt -y install man bash-completion git jq curl build-essential netcat wget psmisc lz4 file net-tools brotli unzip zip moreutils pv tree whois
+
+echo Installing Go
+wget https://golang.org/dl/go1.14.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.14.6.linux-amd64.tar.gz
+rm go1.14.6.linux-amd64.tar.gz
+cat <<'EOF'>> ~/.bashrc
+GOPATH=$HOME/go
+PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+EOF
+
+echo Installing Go tools
+GOPATH=$HOME/go
+PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+go get golang.org/x/tools/cmd/goimports
+
+echo Installing OpenSSH
+sudo apt -y install openssh-client
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ''
+echo "Select & copy (right-click) the following line :"
+cat ~/.ssh/id_ed25519.pub
