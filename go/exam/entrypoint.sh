d0aa10141246a83cc6de0fc2f8b500796b270f6e
xpetit <32063953+xpetit@users.noreply.github.com>
2020-07-30 15:47:12 +0200

    Remove Go formatting check

--- go/exam/entrypoint.sh
+++ go/exam/entrypoint.sh
@@ -13,14 +13,6 @@ first_file=$(echo "$EXPECTED_FILES" | cut -d' ' -f1)
 mkdir -p "$(dirname "$first_file")"
 cat > "$first_file"
 
-# Check formatting
-s=$(goimports -d .)
-if test "$s"; then
-	echo '$ goimports -d .'
-	echo "$s"
-	exit 1
-fi
-
 cd
 
 # Run program
