c002ff2f8ddacbf99760ee85b3544084a8007a5b
xpetit <32063953+xpetit@users.noreply.github.com>
2020-09-25 10:23:29 +0200

    Emergency fix for running a function

--- go/exam/entrypoint.sh
+++ go/exam/entrypoint.sh
@@ -10,11 +10,16 @@ mkdir -p src/student
 cd src/student
 
 first_file=$(echo "$EXPECTED_FILES" | cut -d' ' -f1)
-mkdir -p "$(dirname "$first_file")"
+dir=$(dirname "$first_file")
+mkdir -p "$dir"
 cat > "$first_file"
 
 cd
 
 # Run program
-go build "./src/student/$EXERCISE"
-./"${EXERCISE}" "$@"
+if test "$dir" = "."; then
+    go build -o exe "./src/student"
+else
+    go build -o exe "./src/student/$EXERCISE"
+fi
+./exe "$@"
