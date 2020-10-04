55879d960e8bfff25e6f1a59ec52f1b44cb256d4
xpetit <32063953+xpetit@users.noreply.github.com>
2020-10-04 18:36:08 +0000

    Simplify

--- go/exam/entrypoint.sh
+++ go/exam/entrypoint.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
-set -o noglob
-set -o errexit
-set -o nounset
-IFS='
-'
+set -e
 
-mkdir -p src/student
-cd src/student
-
-first_file=$(echo "$EXPECTED_FILES" | cut -d' ' -f1)
-dir=$(dirname "$first_file")
-mkdir -p "$dir"
-cat > "$first_file"
-
-cd
-
-# Run program
-if test "$dir" = "."; then
-    go build -o exe "./src/student"
-else
-    go build -o exe "./src/student/$EXERCISE"
-fi
+go build -o exe ./student
 ./exe "$@"
