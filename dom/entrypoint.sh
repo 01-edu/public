0b0e9903774f051eff1c1715b443d8c91031d3c5
xpetit <32063953+xpetit@users.noreply.github.com>
2020-10-06 18:31:45 +0000

    Make tester work again (need to fix sh exercises with curl)

--- dom/entrypoint.sh
+++ dom/entrypoint.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 
-set -o noglob
-set -o errexit
-set -o nounset
-IFS='
-'
-
-mkdir student
-cd student
-
-if test "$REPOSITORY"; then
-  password=$(cat)
-  git clone --quiet --depth=1 --shallow-submodules https://root:"${password}"@"$REPOSITORY" .
-else
-  first_file=$(echo "$EXPECTED_FILES" | cut -d' ' -f1)
-  mkdir -p "$(dirname "$first_file")"
-  cat > "$first_file"
-fi
+set -e
 
 cd /app
 node --no-warnings --unhandled-rejections=strict test.js "${EXERCISE}"
