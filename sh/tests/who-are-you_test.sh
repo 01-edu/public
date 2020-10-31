0c9bc5753e27c5d52f13a58f8c1e9756e8733dc0
xpetit <32063953+xpetit@users.noreply.github.com>
2020-10-31 17:27:30 +0000

    Make who-are-you & cl-camp4 exercises work offline

--- sh/tests/who-are-you_test.sh
+++ sh/tests/who-are-you_test.sh
@@ -5,7 +5,12 @@ set -euo pipefail
 IFS='
 '
 
+echo insecure >> ~/.curlrc
+caddy start &>/dev/null
+
 submitted=$(./student/who-are-you.sh)
 expected=$(./solutions/who-are-you.sh)
+
+caddy stop &>/dev/null
 
 diff <(echo "$submitted") <(echo "$expected")
