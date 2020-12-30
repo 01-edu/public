9e4fe188761356c4b8c18bc666871dfa0cf304ec
Augusto <aug.ornelas@gmail.com>
2020-12-30 12:13:31 +0000

    Add usage to exercise `string_literal`

--- rust/tests/string_literal_test/src/main.rs
+++ rust/tests/string_literal_test/src/main.rs
@@ -29,6 +29,14 @@ use jemalloc_ctl::{epoch, stats};
 #[allow(unused_imports)]
 use string_literal::*;
 
+fn main() {
+	println!("{}", is_empty(""));
+	println!("{}", is_ascii("rust"));
+	println!("{}", contains("rust", "ru"));
+	println!("{:?}", split_at("rust", 2));
+	println!("{}", find("rust", 'u'));
+}
+
 #[allow(dead_code)]
 fn is_empty_sol(v: &str) -> bool {
 	v.is_empty()
