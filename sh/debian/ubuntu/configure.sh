f1c559f779687758f2ed0a6ea81fa3a96c2fffcc
xpetit <32063953+xpetit@users.noreply.github.com>
2021-03-02 18:12:37 +0100

    Fix option

--- sh/debian/ubuntu/configure.sh
+++ sh/debian/ubuntu/configure.sh
@@ -225,7 +225,7 @@ sudo -iu student go get github.com/01-edu/z01
 
 apt-get --no-install-recommends -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository -yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository --yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get --no-install-recommends update
 apt-get --no-install-recommends -y install docker-ce docker-ce-cli containerd.io
 adduser student docker
