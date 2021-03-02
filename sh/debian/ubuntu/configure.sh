d8eedf15b71fff6084572ff09902bb4e8f387c60
xpetit <32063953+xpetit@users.noreply.github.com>
2021-03-02 17:47:06 +0100

    Skip confirmation

--- sh/debian/ubuntu/configure.sh
+++ sh/debian/ubuntu/configure.sh
@@ -225,7 +225,7 @@ sudo -iu student go get github.com/01-edu/z01
 
 apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository -yes "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 adduser student docker
