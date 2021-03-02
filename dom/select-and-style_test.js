694f1c5c831585a32edf5c78dc3cad7c9c6831b3
Clement Denis <le.mikmac@gmail.com>
2021-03-02 20:59:51 +0000

    dom: add utilty functions eq.css and eq.$

--- dom/select-and-style_test.js
+++ dom/select-and-style_test.js
@@ -1,24 +1,22 @@
+
+
 export const tests = []
 
-tests.push(async ({ page, eq }) => {
+tests.push(async ({ eq }) => {
   // check the CSS stylesheet is linked in the head tag
-  const CSSLink = await page.$$eval('head', (nodes) =>
-    [...nodes[0].children].some(
-      (node) => node.tagName === 'LINK' && node.rel === 'stylesheet',
-    ),
-  )
-  eq(CSSLink, true)
 
-  // check the universal selector has been declared properly
-  const universalSelectorStyle = await page.evaluate(() => {
-    const target = [...window.document.styleSheets[0].cssRules].find(
-      (rule) => rule.selectorText === '*',
-    )
-    const { margin, opacity, boxSizing } = target.style
-    return { margin, opacity, boxSizing }
+  return eq.$('head link', {
+    rel: 'stylesheet',
+    href: 'http://localhost:9898/select-and-style/select-and-style.css',
   })
-  eq(
-    { margin: '0px', opacity: '0.85', boxSizing: 'border-box' },
-    universalSelectorStyle,
-  )
+})
+
+tests.push(async ({ eq }) => {
+  // check the universal selector has been declared properly
+
+  return eq.css('*', {
+    margin: '0px',
+    opacity: '0.85',
+    boxSizing: 'border-box',
+  })
 })
