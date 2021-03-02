27d9d76b42de783a3d5c373d394d1949d1431cf9
xpetit <32063953+xpetit@users.noreply.github.com>
2021-03-02 14:58:24 +0100

    Fix distribution name

--- sh/debian/ubuntu/configure.sh
+++ sh/debian/ubuntu/configure.sh
@@ -224,8 +224,8 @@ sudo -iu student go get github.com/01-edu/z01
 # Install Docker
 
 apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 
