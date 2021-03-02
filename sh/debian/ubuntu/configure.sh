03102b4ccb9bb3fcad5f82ad4508d9d5e69fd496
xpetit <32063953+xpetit@users.noreply.github.com>
2021-03-02 14:48:05 +0100

    Update Docker version & Docker installation method

--- sh/debian/ubuntu/configure.sh
+++ sh/debian/ubuntu/configure.sh
@@ -223,8 +223,17 @@ sudo -iu student go get github.com/01-edu/z01
 
 # Install Docker
 
-apt-get -y install docker.io
-adduser student docker
+apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# Install Docker compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/1.28.5/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
 
 # Purge unused Ubuntu packages
 pkgs="
