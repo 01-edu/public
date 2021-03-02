e679a8e9b1898c4869f7d501e23e9faaf93bbd37
Louise Foussat <louisefoussat@gmail.com>
2021-03-02 10:40:53 +0000

    update verydisco tests -> execute scripts in tmpdir

--- js/tests/verydisco-forever_test.mjs
+++ js/tests/verydisco-forever_test.mjs
@@ -1,37 +1,45 @@
 import fs from 'fs/promises'
+import { join, resolve } from 'path'
+import { tmpdir } from 'os'
 const readFile = fs.readFile
+const mkdir = fs.mkdir
 import { promisify } from 'util'
 import * as cp from 'child_process'
 
 const exec = promisify(cp.exec)
 
-export const tests = []
 const name = 'verydisco-forever'
-const withArgAndExpect = async ({ arg, expect, path, eq }) => {
-  const cwd = `${path
-    .split('/')
-    .slice(0, -1)
-    .join('/')}/`
-  await exec(`node ${name}.mjs "${arg}"`, { cwd })
-  const out = await readFile(`${cwd}${name}.txt`, 'utf8').catch((err) =>
+export const tests = []
+export const setup = async () => {
+  const dir = tmpdir()
+  // check if already exists and rm?
+  await mkdir(`${dir}/${name}`)
+
+  return { tmpPath: `${dir}/${name}` }
+}
+const withArgAndExpect = async ({ arg, expect, path, eq, ctx }) => {
+  const scriptPath = join(resolve(), path)
+  await exec(`node ${scriptPath} "${arg}"`, { cwd: ctx.tmpPath }) // ${name}.mjs
+  const out = await readFile(`${ctx.tmpPath}/${name}.txt`, 'utf8').catch((err) =>
     err.code === 'ENOENT' ? 'output file not found' : err,
   )
-  await exec(`rm ${name}.txt`, { cwd })
+  await exec(`rm ${name}.txt`, { cwd: ctx.tmpPath })
   return eq(out, expect)
 }
 
-tests.push(async ({ path, eq }) =>
-  withArgAndExpect({ path, eq, arg: 'discovery', expect: 'verydisco' }),
+tests.push(async ({ path, eq, ctx }) =>
+  withArgAndExpect({ path, eq, ctx, arg: 'discovery', expect: 'verydisco' }),
 )
 
-tests.push(async ({ path, eq }) =>
-  withArgAndExpect({ path, eq, arg: 'kiss cool', expect: 'sski olco' }),
+tests.push(async ({ path, eq, ctx }) =>
+  withArgAndExpect({ path, eq, ctx, arg: 'kiss cool', expect: 'sski olco' }),
 )
 
-tests.push(async ({ path, eq }) =>
+tests.push(async ({ path, eq, ctx }) =>
   withArgAndExpect({
     path,
     eq,
+    ctx,
     arg: 'Node is awesome',
     expect: 'deNo si omeawes',
   }),
