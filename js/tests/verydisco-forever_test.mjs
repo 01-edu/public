6f2eefe4330b3dcccd7c6600c7da02758f487883
Louise Foussat <louisefoussat@gmail.com>
2021-03-09 18:04:27 +0000

    update tests for verydisco exercises

--- js/tests/verydisco-forever_test.mjs
+++ js/tests/verydisco-forever_test.mjs
@@ -8,41 +8,66 @@ import * as cp from 'child_process'
 
 const exec = promisify(cp.exec)
 
-const name = 'verydisco-forever'
 export const tests = []
-export const setup = async () => {
-  const dir = tmpdir()
-  // check if already exists and rm?
-  await mkdir(`${dir}/${name}`)
+const randomLetters = (number) =>
+  Math.random()
+    .toString(36)
+    .substring(0, number)
 
-  return { tmpPath: `${dir}/${name}` }
+export const setup = async ({ path }) => {
+  const run = async (word) => {
+    await exec(`node ${path} ${word}`)
+    const fileContent = await readFile('verydisco-forever.txt', 'utf8').catch((err) =>
+      err.code === 'ENOENT' ? 'output file not found' : err,
+    )
+
+    return { data: fileContent }
+  }
+
+  return { run }
 }
-const withArgAndExpect = async ({ arg, expect, path, eq, ctx }) => {
-  const scriptPath = join(resolve(), path)
-  await exec(`node ${scriptPath} "${arg}"`, { cwd: ctx.tmpPath }) // ${name}.mjs
-  const out = await readFile(`${ctx.tmpPath}/${name}.txt`, 'utf8').catch((err) =>
-    err.code === 'ENOENT' ? 'output file not found' : err,
-  )
-  await exec(`rm ${name}.txt`, { cwd: ctx.tmpPath })
-  return eq(out, expect)
-}
 
-tests.push(async ({ path, eq, ctx }) =>
-  withArgAndExpect({ path, eq, ctx, arg: 'discovery', expect: 'verydisco' }),
-)
+tests.push(async ({ ctx, eq }) => {
+  const { data } = await ctx.run(`discovery`)
+  return eq(data, 'verydisco')
+})
 
-tests.push(async ({ path, eq, ctx }) =>
-  withArgAndExpect({ path, eq, ctx, arg: 'kiss cool', expect: 'sski olco' }),
-)
+tests.push(async ({ ctx, eq }) => {
+  const { data } = await ctx.run(`"kiss cool"`)
+  return eq(data, "sski olco")
+})
 
-tests.push(async ({ path, eq, ctx }) =>
-  withArgAndExpect({
-    path,
-    eq,
-    ctx,
-    arg: 'Node is awesome',
-    expect: 'deNo si omeawes',
-  }),
-)
+tests.push(async ({ ctx, eq }) => {
+  const { data } = await ctx.run(`kiss cool`)
+  return eq(data, "sski")
+})
+
+tests.push(async ({ ctx, eq }) => {
+  const { data } = await ctx.run(`"Node is awesome"`)
+  return eq(data, "deNo si omeawes")
+})
+
+tests.push(async ({ ctx, eq }) => {
+  const tic = randomLetters(7)
+  const tac = randomLetters(7)
+  const { data } = await ctx.run(`"${tic}${tac}"`)
+  return eq(data, `${tac}${tic}`)
+})
+
+tests.push(async ({ ctx, eq }) => {
+  const ying = randomLetters(8)
+  const yang = randomLetters(7)
+  const { data } = await ctx.run(`"${ying}${yang}"`)
+  return eq(data, `${yang}${ying}`)
+})
+
+tests.push(async ({ ctx, eq }) => {
+  const tic = randomLetters(5)
+  const tac = randomLetters(5)
+  const ying = randomLetters(3)
+  const yang = randomLetters(2)
+  const { data } = await ctx.run(`"${tic}${tac} ${ying}${yang}"`)
+  return eq(data, `${tac}${tic} ${yang}${ying}`)
+})
 
 Object.freeze(tests)
