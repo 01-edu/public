2bc751a2cc9d2f60b25f6e2e99947be308f0913b
xpetit <32063953+xpetit@users.noreply.github.com>
2021-06-30 18:50:40 +0200

    Debug

--- sh/tests/solutions/to-git-or-not-to-git.sh
+++ sh/tests/solutions/to-git-or-not-to-git.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo "USERNAME: $USERNAME"
 curl -s "https://$DOMAIN/api/graphql-engine/v1/graphql" --data '{"query":"{user(where:{githubLogin:{_eq:\"'$USERNAME'\"}}){id}}"}' | jq '.data.user[0].id'
