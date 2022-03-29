d22a97e513fe762ccaeee1ee8e6493f3a0b69ca2
Christopher Fremond <34804391+Frenchris@users.noreply.github.com>
2022-03-29 16:36:29 +0100

    Update to-git-or-not-to-git.sh

--- sh/tests/solutions/to-git-or-not-to-git.sh
+++ sh/tests/solutions/to-git-or-not-to-git.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+echo test
+echo $DOMAIN
+echo $USERNAME
+
 curl -s "https://$DOMAIN/api/graphql-engine/v1/graphql" --data '{"query":"{user(where:{login:{_eq:\"'$USERNAME'\"}}){id}}"}' | jq '.data.user[0].id'
