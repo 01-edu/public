d780d2648341d848d15db53984433e7dbb19cc2b
Clément <le.mikmac@gmail.com>
2022-04-15 13:01:23 +0100

    Add more test cases for molecules-cells

--- js/tests/molecules-cells_test.js
+++ js/tests/molecules-cells_test.js
@@ -4,9 +4,11 @@ const t = (f) => tests.push(f)
 t(({ eq }) => eq(RNA(''), ''))
 t(({ eq }) => eq(RNA('TAGC'), 'AUCG'))
 t(({ eq }) => eq(RNA(DNA('AUCG')), 'AUCG'))
+t(({ eq }) => eq(RNA(DNA('CAUG')), 'CAUG'))
 
 t(({ eq }) => eq(DNA(''), ''))
 t(({ eq }) => eq(DNA('AUCG'), 'TAGC'))
 t(({ eq }) => eq(DNA(RNA('TAGC')), 'TAGC'))
+t(({ eq }) => eq(DNA(RNA('GCAT')), 'GCAT'))
 
 Object.freeze(tests)
