61e9bcda0fc1bdbba7d1308efc00f1c43c5294bc
Christopher Fremond <34804391+Frenchris@users.noreply.github.com>
2022-07-22 16:33:13 +0100

    Update bring-it-to-life-dom_test.mjs
    reformatting to test an update

--- js/tests/bring-it-to-life-dom_test.mjs
+++ js/tests/bring-it-to-life-dom_test.mjs
@@ -21,7 +21,6 @@ tests.push(async ({ eq, page }) => {
   // await page.setJavaScriptEnabled(true)
   // await page.reload()
   await eq.$('p#eye-left', { className: 'eye eye-closed' })
-
   
   // check the background color of left eye has changed after the JS is loaded
   const eyeLeftBg = await page.$eval(
