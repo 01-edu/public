b1571a898ec4343cadc15a843c4d0586f45a57a2
miguel <miguel-silva98@hotmail.com>
2022-08-12 09:38:28 +0100

    Fix(removing echos from the solution)

--- sh/tests/solutions/to-git-or-not-to-git.sh
+++ sh/tests/solutions/to-git-or-not-to-git.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-echo WHAT
-response=$(curl -s "https://$DOMAIN/api/graphql-engine/v1/graphql" --data '{"query":"{user(where:{login:{_eq:\"'$USERNAME'\"}}){id}}"}' | jq '.data.user[0].id')
-echo RESPONSE: $response
+curl -s "https://$DOMAIN/api/graphql-engine/v1/graphql" --data '{"query":"{user(where:{login:{_eq:\"'$USERNAME'\"}}){id}}"}' | jq '.data.user[0].id'
