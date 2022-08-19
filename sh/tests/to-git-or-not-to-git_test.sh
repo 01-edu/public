632a735506abcb11cf62627f06fb447aafee0edc
miguel <miguel-silva98@hotmail.com>
2022-08-19 15:04:14 +0100

    test(to-git-or-not-to-git):Adding the exit status to the test

--- sh/tests/to-git-or-not-to-git_test.sh
+++ sh/tests/to-git-or-not-to-git_test.sh
@@ -6,17 +6,19 @@ IFS='
 '
 
 FILENAME="student/to-git-or-not-to-git.sh"
-submitted=$(bash student/to-git-or-not-to-git.sh)
-expected=$(bash solutions/to-git-or-not-to-git.sh)
 
 # True if FILE exists and is a regular file
 if [ -f ${FILENAME} ]; then
     # FILE exists and it's not empty
     if [ -s ${FILENAME} ]; then
+        submitted=$(bash $FILENAME)
+        expected=$(bash solutions/to-git-or-not-to-git.sh)
         diff <(echo "$submitted") <(echo "$expected")
     else
         echo "The file exist but is empty"
+        exit 1
     fi
 else
     echo "File does not exist"
+    exit 1
 fi
