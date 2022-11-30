f253a199d7008c09a6aa936a368ca2adbec3e5bd
miguel <miguel-silva98@hotmail.com>
2022-11-30 17:34:07 +0000

    feat(count-files): add subject, test and solution for the exercise count-files

---
+++ sh/tests/count-files_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
+
+challenge() {
+    submitted=$(cd "$1" && bash "$script_dirS"/student/count-files.sh)
+    expected=$(cd "$1" && bash "$script_dirS"/solutions/count-files.sh)
+
+    diff <(echo "$submitted") <(echo "$expected")
+}
+
+challenge cl-camp5/folder1
+challenge cl-camp5/folder2
