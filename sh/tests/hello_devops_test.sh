55fcf084aeaba41ff923f3779cc6b4162ad9cd04
Zouhair AMAZZAL <zouhair.amazzal@01talent.com>
2022-12-13 11:55:37 +0100

    DEV-3896 feat(hello_devops) add subject and tests for into exercise to teach student how to push in gitea repo

---
+++ sh/tests/hello_devops_test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+FILENAME="student/hello_devops.sh"
+
+# True if FILE exists and is a regular file
+if [ -f ${FILENAME} ]; then
+    # FILE exists and it's not empty
+    if [ -s ${FILENAME} ]; then
+        submitted=$(bash $FILENAME)
+        expected=$(bash solutions/hello_devops.sh)
+        diff <(echo "$submitted") <(echo "$expected") | cat -t
+    else
+        echo "The file exist but is empty"
+        exit 1
+    fi
+else
+    echo "File does not exist"
+    exit 1
+fi
