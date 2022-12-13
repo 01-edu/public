9b975fdf0fd8f63d6d70f109187032a046f620a9
miguel <miguel-silva98@hotmail.com>
2022-12-13 18:24:41 +0000

    feat(hard-perms): add subject, test and solution for the exercise

---
+++ sh/tests/solutions/hard-perm.sh
@@ -0,0 +1,1 @@
+chmod 401 hard-perm/0 hard-perm/A && chmod 402 hard-perm/1 hard-perm/9 && chmod 604 hard-perm/2 hard-perm/8 && chmod 777 hard-perm/3 && chmod 510 hard-perm/4 hard-perm/7 && chmod 460 hard-perm/5 hard-perm/6
