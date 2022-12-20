eb6e281da04a91367c04e78f5949341bc6c3f68e
miguel <miguel-silva98@hotmail.com>
2022-12-20 15:32:27 +0000

    feat(file-details): add subject, test and solution for the exercise

---
+++ sh/tests/file-details_test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
+
+challenge() {
+    submitted=$(cd "$1" && bash "$script_dirS"/student/file-details.sh)
+    expected=$(cd "$1" && bash "$script_dirS"/solutions/file-details.sh)
+
+    diff <(echo "$submitted") <(echo "$expected")
+}
+
+challenge hard-perm
---
+++ sh/tests/solutions/file-details.sh
@@ -0,0 +1,1 @@
+TZ=utc ls -l --time-style='+%F %R' | sed 1d | awk '{print $1, $6, $7, $8, $9, $10}'
