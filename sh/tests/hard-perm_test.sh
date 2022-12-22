526f60501c30fba180165c403c556e6fb616aa11
miguel <miguel-silva98@hotmail.com>
2022-12-22 16:26:03 +0000

    fix(hard-perms): fix test to check only the perms

--- sh/tests/hard-perm_test.sh
+++ sh/tests/hard-perm_test.sh
@@ -9,8 +9,8 @@ script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
 chmod 303 hard-perm/*
 
 challenge() {
-    submitted=$(bash "$script_dirS"/student/hard-perm.sh && ls -l "$1")
-    expected=$(bash "$script_dirS"/solutions/hard-perm.sh && ls -l "$1")
+    submitted=$(bash "$script_dirS"/student/hard-perm.sh && ls -l "$1" | awk '{print $1}')
+    expected=$(bash "$script_dirS"/solutions/hard-perm.sh && ls -l "$1" | awk '{print $1}')
     diff <(echo "$submitted") <(echo "$expected")
 }
 
