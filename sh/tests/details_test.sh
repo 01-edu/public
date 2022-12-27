e298d48ed9f605ece5487e617726698e707ff3a6
miguel <miguel-silva98@hotmail.com>
2022-12-27 14:58:29 +0000

    feat(details): add subject, test and solution for the exercise

---
+++ sh/tests/details_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
+challenge() {
+    touch file1.txt
+    submitted=$(bash "$script_dirS"/student/details.sh && ls -l file1.txt | awk '{print $1, $2, $5, $6, $7, $8, $9}')
+    expected=$(bash "$script_dirS"/solutions/details.sh && ls -l file1.txt | awk '{print $1, $2, $5, $6, $7, $8, $9}')
+    diff <(echo "$submitted") <(echo "$expected")
+    stat file1.txt
+}
+
+challenge
+rm file1.txt
