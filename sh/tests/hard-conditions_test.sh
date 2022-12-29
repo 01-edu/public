64644e208e21c74e9f9233568f0379dc6c78b717
miguel <miguel-silva98@hotmail.com>
2022-12-29 16:26:59 +0000

    fix(hard-conditions): adding test case for nos executable

--- sh/tests/hard-conditions_test.sh
+++ sh/tests/hard-conditions_test.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 IFS='
 '
 script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
-cd "$script_dirS"
-submitted=$(bash "$script_dirS"/student/hard-conditions.sh hard-conditions_test.sh)
-expected=$(bash "$script_dirS"/solutions/hard-conditions.sh hard-conditions_test.sh)
+submitted=$(bash "$script_dirS"/student/hard-conditions.sh hard-conditions/test-hard-conditions.sh)
+expected=$(bash "$script_dirS"/solutions/hard-conditions.sh hard-conditions/test-hard-conditions.sh)
+diff <(echo "$submitted") <(echo "$expected")
+submitted=$(bash "$script_dirS"/student/hard-conditions.sh hard-conditions/non-ex)
+expected=$(bash "$script_dirS"/solutions/hard-conditions.sh hard-conditions/non-ex)
 diff <(echo "$submitted") <(echo "$expected")
