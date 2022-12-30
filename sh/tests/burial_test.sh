6ce50f2bde9b701799b98be7f6e00476ba172637
miguel <miguel-silva98@hotmail.com>
2022-12-30 12:41:19 +0000

    feat(burial): add subject, test and solution for the exercise

---
+++ sh/tests/burial_test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+FILENAME="student/burial.sh"
+
+# True if FILE exists and is a regular file
+if [ -f ${FILENAME} ]; then
+    # FILE exists and it's not empty
+    if [ -s ${FILENAME} ]; then
+        if [[ $(cat $FILENAME | grep echo | wc -l) -ne 0 ]]; then
+            echo "echo is not allowed in this exercise!"
+            exit 1
+        fi
+        submitted=$(bash $FILENAME)
+        expected=$(bash solutions/burial.sh)
+        diff <(echo "$submitted") <(echo "$expected")
+    else
+        echo "The file exist but is empty"
+        exit 1
+    fi
+else
+    echo "File does not exist"
+    exit 1
+fi
