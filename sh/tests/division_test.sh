d50afde56abf83a2be6f4c5ac034676f03c19523
eslopfer <lopezesteban8@gmail.com>
2023-01-05 11:31:48 +0000

    test(division): fix tests

--- sh/tests/division_test.sh
+++ sh/tests/division_test.sh
@@ -14,10 +14,10 @@ challenge() {
     diff <(echo "$submitted") <(echo "$expected")
 }
 
-challenge "10 2" "5"
-challenge "4 2" "2"
-challenge "0.5 0.5" "1"
-challenge "0.5" "Error: two numbers must be provided"
-challenge "foo bar" "Error: both arguments must be numeric"
-challenge "1 0" "Error: division by zero is not allowed"
-challenge "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 2" "5000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+challenge "10" "2"
+challenge "4" "2"
+challenge "0.5" "0.5"
+challenge "0.5"
+challenge "foo" "bar"
+challenge "1" "0"
+challenge "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" "2"
