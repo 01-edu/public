5558a83c62b70209b9ed15d30d75c76939277968
eslopfer <lopezesteban8@gmail.com>
2023-01-05 16:54:24 +0000

    feat(file-checker): add solution for exercise

---
+++ sh/tests/solutions/file-checker.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Check if the script was given an argument
+if [ $# -ne 1 ]
+then
+    echo "Error: a file must be provided as an argument"
+    exit 1
+fi
+
+# Check if file exists
+if [ ! -e "$1" ]
+then
+    echo "File does not exist"
+else
+    echo "File exists"
+fi
+
+# Check file's permissions
+if [ -r "$1" ]
+then
+    echo "File is readable"
+else
+    echo "File is not readable"
+fi
+
+if [ -w "$1" ]
+then
+    echo "File is writable"
+else
+    echo "File is not writable"
+fi
+
+if [ -x "$1" ]
+then
+    echo "File is executable"
+else
+    echo "File is not executable"
+fi
