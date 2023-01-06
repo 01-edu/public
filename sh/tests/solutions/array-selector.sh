c231898bcabbcddd069250b77a88f982ce464b83
nprimo <primo.niccolo@gmail.com>
2023-01-06 13:33:44 +0100

    fix(array-selector): remove exit command from solution

--- sh/tests/solutions/array-selector.sh
+++ sh/tests/solutions/array-selector.sh
@@ -6,9 +6,9 @@ IFS='
 COLORS=('red' 'blue' 'green' 'white' 'black')
 if [[ ! $1 =~ ^[0-9]+$ ]] || [[ $1 -le 0 ]] || [[ $1 -gt ${#COLORS[@]} ]]; then
     echo 'Error'
-    exit 1
+else
+	POS=$(($1 - 1))
+	echo ${COLORS[$POS]}
 fi
 
-POS=$(($1 - 1))
-echo ${COLORS[$POS]}
 
