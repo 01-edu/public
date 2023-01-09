438b0cfa773d3d4637a0667a1217ea4dcb4d9bd3
nprimo <primo.niccolo@gmail.com>
2023-01-09 18:18:11 +0000

    feat(remake): add subject, tests and solution

---
+++ sh/tests/remake_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+IFS='
+'
+
+challenge () {
+    if [[ $# -eq 1 ]]; then
+        submitted=$(bash student/remake.sh "$1")
+        expected=$(bash solutions/remake.sh "$1"-expected)
+        diff <(echo $submitted) <(echo $expected)
+        diff <(ls -ltr $1) <(ls -ltr $1-expected)
+    else
+        diff <(bash student/remake.sh "$@") <(bash solutions/remake.sh "$@")
+    fi
+}
+
+challenge a
+challenge 
