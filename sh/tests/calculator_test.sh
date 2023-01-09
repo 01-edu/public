b7af22ebf26bc6681f22d50b3b2d0eacf15ec03a
Michele Sessa <mikysett@gmail.com>
2023-01-09 15:52:41 +0000

    feat(calculator): add new exercise to scripting piscine

---
+++ sh/tests/calculator_test.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# set -euo pipefail
+IFS='
+'
+
+script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
+
+challenge() {
+    submitted="./calculator.sh $@
+    "
+    expected="./calculator.sh $@
+    "
+    submitted+=$(2>&1 bash "$script_dirS"/student/calculator.sh "$@")
+    submitted+="
+    exit status: $?"
+    expected+=$(2>&1 bash "$script_dirS"/solutions/calculator.sh "$@")
+    expected+="
+    exit status: $?"
+
+    diff -U 1 <(echo "$submitted") <(echo "$expected")
+    if [ $? != 0 ]
+    then
+        exit 1
+    fi
+}
+
+# Valid inputs
+challenge "15" "+" "10"
+challenge "15" "-" "10"
+challenge "15" "/" "10"
+challenge "15" "*" "10"
+
+challenge "3491" "+" "-67"
+challenge "3491" "-" "-67"
+challenge "3491" "/" "-67"
+challenge "3491" "*" "-67"
+
+challenge "-3491" "+" "-67"
+challenge "-3491" "-" "-67"
+challenge "-3491" "/" "-67"
+challenge "-3491" "*" "-67"
+
+# Invalid inputs
+
+challenge
+challenge "-3491" "*" "-67" "10" "12"
+
+challenge "20" "/" "0"
+challenge "20" "@" "10"
+challenge "10" "*" "67invalid"
+
+# Test operators functions
+
+source $script_dirS"/student/calculator.sh" 10 + 10 >/dev/null 2>&1
+
+if [ $(do_add 11 14) != 25 ]
+then
+    echo "error in function do_add"
+    exit 1
+fi
+
+if [ $(do_sub 11 14) != -3 ]
+then
+    echo "error in function do_sub"
+    exit 1
+fi
+
+if [ $(do_mult 3 5) != 15 ]
+then
+    echo "error in function do_mult"
+    exit 1
+fi
+
+if [ $(do_divide 50 5) != 10 ]
+then
+    echo "error in function do_divide"
+    exit 1
+fi
