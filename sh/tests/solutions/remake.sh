f1a7dd0d2b37b75ea850b86c6b427058d13f45fd
nprimo <primo.niccolo@gmail.com>
2023-01-09 18:18:11 +0000

    feat(remake): add subject, tests and solution

---
+++ sh/tests/solutions/remake.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+IFS='
+'
+
+if [[ $# -eq 1 ]]; then
+    mkdir -p $1
+    cd $1
+    touch -t 01010001 ciao
+    chmod 442 ciao
+    mkdir mamma
+    touch -t 01020001 mamma
+    chmod 777 mamma
+    touch -t 01030001 guarda
+    chmod 400 guarda
+    touch -t 01040001 come
+    chmod 642 come
+    mkdir mi
+    touch -t 01050001 mi
+    chmod 452 mi
+    touch -t 01060001 diverto
+    chmod 421 diverto
+else
+    echo Error
+    exit 1
+fi
