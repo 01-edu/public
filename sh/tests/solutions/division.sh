3b29dba3a8c4e307940686e40c1839a4aefc0dfc
eslopfer <lopezesteban8@gmail.com>
2023-01-09 12:30:58 +0000

    refactor(division): modify solution to avoid having to exit

--- sh/tests/solutions/division.sh
+++ sh/tests/solutions/division.sh
@@ -4,25 +4,21 @@
 if [ $# -ne 2 ]
 then
     echo "Error: two numbers must be provided"
-    exit 0
-fi
 
 # # Check if the arguments are numeric
-if ! [[ $1 =~ ^-?[0-9]*\.?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]*\.?[0-9]+$ ]]
+elif ! [[ $1 =~ ^-?[0-9]*\.?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]*\.?[0-9]+$ ]]
 then
     echo "Error: both arguments must be numeric"
-    exit 0
-fi
 
 # Check if the second argument is not 0
-if [ $(echo "$2 == 0" | bc) -eq 1 ]
+elif [ $(echo "$2 == 0" | bc) -eq 1 ]
 then
     echo "Error: division by zero is not allowed"
-    exit 0
-fi
 
 # Divide the first argument by the second using bc
+else 
 result=$(echo "$1 / $2" | bc )
 
 # Output the result
 echo $result
+fi
