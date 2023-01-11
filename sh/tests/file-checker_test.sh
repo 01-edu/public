0f95569f9edbdcb8520efcb4346e4f6fe446b2db
eslopfer <lopezesteban8@gmail.com>
2023-01-11 15:28:45 +0000

    test(file-checker): refactor test for robustness

--- sh/tests/file-checker_test.sh
+++ sh/tests/file-checker_test.sh
@@ -3,18 +3,23 @@
 set -euo pipefail
 IFS='
 '
-
 script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
 
-submitted=$(bash "$script_dirS"/student/file-checker.sh "$script_dirS"/student/hello-devops.sh)
-expected=$(bash "$script_dirS"/solutions/file-checker.sh "$script_dirS"/student/hello-devops.sh)
-
 # check that test command was not used
-
 if grep -q "test" "$script_dirS"/student/file-checker.sh
 then
     echo "The 'test' command is not allowed in this exercise"
     exit 1
 fi
 
-diff <(echo "$submitted") <(echo "$expected")
+challenge() {
+    submitted=$(bash "$script_dirS"/student/file-checker.sh "$1")
+    expected=$(bash "$script_dirS"/solutions/file-checker.sh "$1")
+
+    diff <(echo "$submitted") <(echo "$expected")
+}
+
+challenge "./file-checker/readable-only"
+challenge "./file-checker/readable-and-writable"
+challenge "./file-checker/readable-executable"
+challenge "./file-checker/readable-writable-executable"
