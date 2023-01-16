5f517ea08f246fc4a258708b38a1e38dbc7e8deb
miguel <miguel-silva98@hotmail.com>
2023-01-16 18:32:14 +0000

    fix(left): fixing test

--- sh/tests/left_test.sh
+++ sh/tests/left_test.sh
@@ -9,22 +9,16 @@ script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
 FILENAME="../student/left.sh"
 cd left
 
-# True if FILE exists and is a regular file
-if [ -f ${FILENAME} ]; then
-    # FILE exists and it's not empty
-    if [ -s ${FILENAME} ]; then
-        if [[ $(cat $FILENAME | grep echo | wc -l) -ne 0 ]]; then
-            echo "echo is not allowed in this exercise!"
-            exit 1
-            submitted=$(bash $FILENAME)
-            expected=$(bash ../solutions/left.sh)
-            diff <(echo "$submitted") <(echo "$expected")
-        fi
-    else
-        echo "The file exist but is empty"
-        exit 1
-    fi
-else
-    echo "File does not exist"
+if [[ $(cat $FILENAME | grep echo | wc -l) -ne 0 ]]; then
+    echo "echo is not allowed in this exercise!"
     exit 1
 fi
+
+if [[ $(cat $FILENAME | grep '<' | wc -l) -lt 1 ]]; then
+    echo "The file does not contain the required redirection"
+    exit 1
+fi
+
+submitted=$(bash $FILENAME)
+expected=$(bash ../solutions/left.sh)
+diff <(echo "$submitted") <(echo "$expected")
