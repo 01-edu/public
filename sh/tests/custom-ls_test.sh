2b77c87031b7a1e94212f8f16dfeb1620a93886d
Zouhair AMAZZAL <44241862+zamazzal@users.noreply.github.com>
2023-01-16 12:56:59 +0100

    DEV-3919-my-ls (#1660)
    * DEV-3919 feat(my-ls) add subject and tests for my-ls project
    
    * DEV-3919 fix(custom-ls) change the name of the exercise from my-ls to custom-ls
    
    * DEV-3919 docs(custom-ls) fix grammar errors
    
    * DEV-3919 test(custom-ls) fix unalias in tests
    
    * DEV-3919 docs(custom-ls) add permanently alias teaching
    
    * test(custom-ls): fix test not able to run aliases
    
    Co-authored-by: Zouhair AMAZZAL <zouhair.amazzal@01talent.com>
    Co-authored-by: Michele Sessa <mikysett@gmail.com>

---
+++ sh/tests/custom-ls_test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+shopt -s expand_aliases
+
+FILENAME="student/custom-ls.sh"
+script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
+
+challenge() {
+
+   	source "$script_dirS"/$FILENAME
+    submitted=$(cd "$1" && custom-ls)
+    unalias custom-ls
+
+	source "$script_dirS"/solutions/custom-ls.sh
+    expected=$(cd "$1" && custom-ls)
+    unalias custom-ls
+
+	diff <(echo "$submitted") <(echo "$expected")
+}
+
+# True if FILE exists and is a regular file
+if [ -f ${FILENAME} ]; then
+    # FILE exists and it's not empty
+    if [ -s ${FILENAME} ]; then
+        if [[ $(cat $FILENAME | grep echo | wc -l) -ne 0 ]]; then
+            echo "echo is not allowed in this exercise!"
+            exit 1
+        fi
+        challenge custom-ls/folder1
+    else
+        echo "The file exist but is empty"
+        exit 1
+    fi
+else
+    echo "File does not exist"
+    exit 1
+fi
