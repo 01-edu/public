52d40b42890211d2a11d9df0762fcc1c11023aa8
nprimo <primo.niccolo@gmail.com>
2023-01-16 09:26:50 +0100

    docs(remake): improve subject
    - run prettier to fix formatting
    - make example generic with `user`
    - improve clarity on exercise objective
    - remove created files after tests are run to allow consecutive
    tests executions without errors

--- sh/tests/remake_test.sh
+++ sh/tests/remake_test.sh
@@ -9,10 +9,11 @@ challenge () {
         expected=$(bash solutions/remake.sh "$1"-expected)
         diff <(echo $submitted) <(echo $expected)
         diff <(ls -ltr $1) <(ls -ltr $1-expected)
+        rm -rf "$1" "$1-expected"
     else
         diff <(bash student/remake.sh "$@") <(bash solutions/remake.sh "$@")
     fi
 }
 
-challenge a
+challenge remake
 challenge 
