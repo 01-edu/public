deb94e37f3d2563a7f0d709d3a99a98258f1c48f
sagarishere <5121817+sagarishere@users.noreply.github.com>
2023-01-17 17:39:22 +0200

    covering -0 cases as well
    as Math.round will give -0 when input is -0.01, it is an interesting behavioral nuance of Javascript that should be fun to cover as well.

--- js/tests/cut-corners_test.js
+++ js/tests/cut-corners_test.js
@@ -2,7 +2,7 @@ Math.round = Math.ceil = Math.floor = Math.trunc = undefined
 // /*/ // ⚡
 export const tests = []
 const t = (f) => tests.push(f)
-const nums = [Math.PI, -Math.PI, Math.E, -Math.E, 0]
+const nums = [Math.PI, -Math.PI, Math.E, -Math.E, 0, -0.01]
 
 t(({ code }) => !/String|['"`]|toFixed|slice/.test(code))
 t(({ code }) => !code.includes('~'))
@@ -12,10 +12,10 @@ t(({ code }) => !/[^|]\|[^|]/.test(code))
 t(({ code }) => !/[^&]&[^&]/.test(code))
 t(({ code }) => !code.includes('parseInt'))
 
-t(({ eq }) => eq(nums.map(round), [3, -3, 3, -3, 0]))
-t(({ eq }) => eq(nums.map(floor), [3, -4, 2, -3, 0]))
-t(({ eq }) => eq(nums.map(trunc), [3, -3, 2, -2, 0]))
-t(({ eq }) => eq(nums.map(ceil), [4, -3, 3, -2, 0]))
+t(({ eq }) => eq(nums.map(round), [3, -3, 3, -3, 0, -0]))
+t(({ eq }) => eq(nums.map(floor), [3, -4, 2, -3, 0, -1]))
+t(({ eq }) => eq(nums.map(trunc), [3, -3, 2, -2, 0, -0]))
+t(({ eq }) => eq(nums.map(ceil), [4, -3, 3, -2, 0, -0]))
 t(({ ctx }) => trunc(0xfffffffff + ctx) === 0xfffffffff + ~~ctx)
 
 export const setup = () => Math.random() * 10
