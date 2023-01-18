a1a27fe0f524d71fa9bbc5b41c32612c46be1cae
eslopfer <lopezesteban8@gmail.com>
2023-01-18 15:51:08 +0000

    test(file-cheker): create test folder and delete it from test file

--- sh/tests/file-checker_test.sh
+++ sh/tests/file-checker_test.sh
@@ -12,6 +12,18 @@ then
     exit 1
 fi
 
+if test ! -e file-checker; then
+    mkdir file-checker
+    cd file-checker
+    touch readable-and-writable readable-only readable-and-executable readable-writable-executable
+    chmod -x "$script_dirS/file-checker/readable-and-writable"
+    chmod -xw "$script_dirS/file-checker/readable-only"
+    chmod -w "$script_dirS/file-checker/readable-and-executable"
+    chmod +x "$script_dirS/file-checker/readable-and-executable"
+    chmod +x "$script_dirS/file-checker/readable-writable-executable"
+    cd ..
+fi
+
 challenge() {
     submitted=$(bash "$script_dirS"/student/file-checker.sh "$1")
     expected=$(bash "$script_dirS"/solutions/file-checker.sh "$1")
@@ -19,12 +31,8 @@ challenge() {
     diff <(echo "$submitted") <(echo "$expected")
 }
 
-chmod -xw "$script_dirS/file-checker/readable-only"
 challenge "$script_dirS/file-checker/readable-only"
-chmod -x "$script_dirS/file-checker/readable-and-writable"
 challenge "$script_dirS/file-checker/readable-and-writable"
-chmod -w "$script_dirS/file-checker/readable-and-executable"
-chmod +x "$script_dirS/file-checker/readable-and-executable"
 challenge "$script_dirS/file-checker/readable-and-executable"
-chmod +x "$script_dirS/file-checker/readable-writable-executable"
 challenge "$script_dirS/file-checker/readable-writable-executable"
+rm -rf file-checker
