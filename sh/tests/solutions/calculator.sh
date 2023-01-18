dc8002d92e67b7b6aadac04578001b734288ad29
Michele <62351258+mikysett@users.noreply.github.com>
2023-01-18 14:50:34 +0000

    DEV-3951-calculator (#1706)
    * feat(calculator): add new exercise to scripting piscine
    
    * test(calculator): fix swapped exit codes for error handling
    
    * docs(calculator): clarify function behavior for errors
    
    * test(calculator): update error message for invalid number
    
    * docs(calculator): add explicit case statement requirement
    
    improve references section style
    add case and getent examples
    
    * fix(calculator): remove getent info in the subject
    
    * chore(calculator): make the solution executable
    
    * test(calculator): add check for case usage
    
    * test(calculator): fix case statement usage check on wrong file
    
    * fix(calculator): remove script added by error
    
    revert changes committed by mistake on Dockerfile and entrypoint.sh
    
    * style(calculator): add newline at end of file

---
+++ sh/tests/solutions/calculator.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS='
+'
+
+number='^-?[0-9]+$'
+
+do_add () {
+    echo $(($1 + $2))
+}
+
+do_sub () {
+    echo $(($1 - $2))
+}
+
+do_mult () {
+    echo $(($1 * $2))
+}
+
+do_divide () {
+    echo $(($1 / $2))
+}
+
+
+if [ $# != 3 ]
+then
+    >&2 echo "Error: expect 3 arguments"
+    exit 1
+elif ! [[ $1 =~ $number && $3 =~ $number ]]
+then
+    >&2 echo "Error: invalid number"
+    exit 4
+else
+    case $2 in
+
+        "+")
+            echo $(do_add $1 $3)
+            ;;
+        
+        "-")
+            echo $(do_sub $1 $3)
+            ;;
+        
+        "*")
+            echo $(do_mult $1 $3)
+            ;;
+        
+        "/")
+            if [ $3 == 0 ]
+            then
+                >&2 echo "Error: division by 0"
+                exit 2
+            fi
+            echo $(do_divide $1 $3)
+            ;;
+        
+        *)
+            >&2 echo "Error: invalid operator"
+            exit 3
+            ;;
+
+    esac
+
+fi
