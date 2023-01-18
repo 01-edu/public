a128062f29e3fd2e346867474c10c6c133435b15
Michele Sessa <mikysett@gmail.com>
2023-01-18 19:29:10 +0000

    feat(numerical-operations-the-return): add new python exercise to scripting piscine

---
+++ sh/tests/solutions/numerical_operations_the_return.py
@@ -0,0 +1,19 @@
+def modulo(a, b):
+    if b == 0:
+        return 0
+    else:
+        return a % b
+
+
+def divide(a, b):
+    if b == 0:
+        return 0
+    else:
+        return a / b
+
+
+def integer_division(a, b):
+    if b == 0:
+        return 0
+    else:
+        return a // b
