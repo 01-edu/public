81047ded89c73df6baf58b96d5688788707c1ee4
eslopfer <lopezesteban8@gmail.com>
2023-01-19 15:40:03 +0000

    test(clen_the_list): add tests for exercise

---
+++ sh/tests/clean_the_list_test.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.path.append('student')
+
+from shopping import clean_list
+
+def test_normal_input():
+    shopping_list = ['book', 'pen', 'milk', 'bread']
+    expected_output = ['1/ Book', '2/ Pen', '3/ Milk', '4/ Bread']
+    assert clean_list(shopping_list) == expected_output
+
+def test_empty_input():
+    shopping_list = []
+    expected_output = []
+    assert clean_list(shopping_list) == expected_output
+
+def test_multiple_spaces():
+    shopping_list = ['    shirt    ', '  pants  ', 'milk', 'shoes']
+    expected_output = ['1/ Shirt', '2/ Pants', '3/ Milk', '4/ Shoes']
+    assert clean_list(shopping_list) == expected_output
+  
+def test_missing_milk():
+    shopping_list = ['computer', 'chair', 'desk']
+    expected_output = ['1/ Computer', '2/ Chair', '3/ Desk', '4/ Milk']
+    assert clean_list(shopping_list) == expected_output
+
+def test_word_with_space():
+    shopping_list = ['tooth brush', 'hand sanitizer', 'milk', 'toilet paper']
+    expected_output = ['1x/ Tooth brush', '2x/ Hand sanitizer', '3x/ Milk', '4x/ Toilet Paper']
+    assert clean_list(shopping_list) == expected_output
