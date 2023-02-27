5c3303f0b70c02d189ebac81b493d68d207daeab
Christopher Fremond <34804391+Frenchris@users.noreply.github.com>
2023-02-27 10:50:29 +0000

    update(atos-script): adds the installation of chrome

--- sh/debian/ubuntu/configure-atos.sh
+++ sh/debian/ubuntu/configure-atos.sh
@@ -155,6 +155,12 @@ done
 
 apt-get --no-install-recommends -y install libreoffice
 
+# Install Chrome
+
+wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
 # End of script reached
 
 # set bashrc path
