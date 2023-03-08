60f60845cfb61ffa977a0dcda38fd0a17e0d8685
nprimo <primo.niccolo@gmail.com>
2023-03-08 15:42:29 +0000

    feat(kept-promise): add test

--- js/tests/kept-promise_test.js
+++ js/tests/kept-promise_test.js
@@ -1,6 +1,19 @@
-import keptPromise 
+export const tests = []
 
-const getImportantInfo = _ =>
-  new Promise(resolve => {
-    setTimeout(_ => resolve(Math.round(Math.random() * 10)), 1000)
-  })
+const testZero = async _ => {
+  return (await processInfo(_ => new Promise(resolve => resolve(0)))) === 'Ok!'
+}
+
+const testOddNumber = async _ => {
+  return (
+    (await processInfo(_ => new Promise(resolve => resolve(1)))) === 'Error!'
+  )
+}
+
+const testEvenNumber = async _ => {
+  return (await processInfo(_ => new Promise(resolve => resolve(4)))) === 'Ok!'
+}
+
+tests.push(testZero, testOddNumber, testEvenNumber)
+
+Object.freeze(tests)
