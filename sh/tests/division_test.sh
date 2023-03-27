48a3186b2641c4293900e4dbe1d6fe33a4e00dd8
miguel <miguel-silva98@hotmail.com>
2023-03-27 18:14:08 +0100

    fix(division): add the bc to the dockerfile add the double square brackets in the solution fix indent in the test

--- sh/tests/division_test.sh
+++ sh/tests/division_test.sh
@@ -9,20 +9,18 @@ script_dirS=$(cd -P "$(dirname "$BASH_SOURCE")" &>/dev/null && pwd)
 
 challenge() {
     # Test if test command was used
-    if grep -q "test" "$script_dirS"/student/division.sh
-    then
+    if grep -q "test" "$script_dirS"/student/division.sh; then
         echo "Error: the test command cannot be used in the student script"
         return
     fi
 
     # Test with one or two arguments
-    if [ $# -eq 1 ]
-    then
-    submitted=$(bash "$script_dirS"/student/division.sh $1)
-    expected=$(bash "$script_dirS"/solutions/division.sh $1)
+    if [ $# -eq 1 ]; then
+        submitted=$(bash "$script_dirS"/student/division.sh $1)
+        expected=$(bash "$script_dirS"/solutions/division.sh $1)
     else
-    submitted=$(bash "$script_dirS"/student/division.sh $1 $2)
-    expected=$(bash "$script_dirS"/solutions/division.sh $1 $2)
+        submitted=$(bash "$script_dirS"/student/division.sh $1 $2)
+        expected=$(bash "$script_dirS"/solutions/division.sh $1 $2)
     fi
     diff <(echo "$submitted") <(echo "$expected")
 }
