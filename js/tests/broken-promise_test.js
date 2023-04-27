3d291c9b6926cf0295fdf7130fd83bc65eb7b4bc
davhojt <davhojt@gmail.com>
2023-04-27 16:44:37 +0300

    feat(broken-promise): add test

---
+++ js/tests/broken-promise_test.js
@@ -0,0 +1,25 @@
+export const tests = []
+
+const testOne = async () => {
+  return (await processInfo(() => new Promise(resolve => resolve(1)))) === 2
+}
+
+const testFifteen = async () => {
+  return (await processInfo(() => new Promise(resolve => resolve(15)))) === 30
+}
+
+const testRejectNoNumbers = async () => {
+  return (
+    (await processInfo(() => new Promise((resolve, reject) => reject("No numbers")))) === "Error: No numbers"
+  )
+}
+
+const testRejectNumberless = async () => {
+  return (
+    (await processInfo(() => new Promise((resolve, reject) => reject("Numberless")))) === "Error: Numberless"
+  )
+}
+
+tests.push(testOne, testFifteen, testRejectNoNumbers, testRejectNumberless)
+
+Object.freeze(tests)
