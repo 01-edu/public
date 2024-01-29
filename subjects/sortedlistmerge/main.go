9377921fd2d9e110866b23c18e2795b646e6099b
miguel <miguel-silva98@hotmail.com>
2024-01-29 16:39:36 +0000

    fix(checkpoint) add imports and functions needed

--- subjects/sortedlistmerge/main.go
+++ subjects/sortedlistmerge/main.go
@@ -5,14 +5,9 @@ import (
 	"piscine"
 )
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
+type NodeI struct {
+	Data int
+	Next *NodeI
 }
 
 func PrintList(l *piscine.NodeI) {
