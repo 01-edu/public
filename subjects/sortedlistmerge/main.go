b1f350d38064d998b8757f73bb7ec26e3625d63b
miguel <miguel-silva98@hotmail.com>
2024-01-29 11:59:47 +0000

    fix(checkpoint) add files to the readme used in the checkpoint

---
+++ subjects/sortedlistmerge/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func PrintList(l *piscine.NodeI) {
+	it := l
+	for it != nil {
+		fmt.Print(it.Data, " -> ")
+		it = it.Next
+	}
+	fmt.Print(nil, "\n")
+}
+
+func listPushBack(l *piscine.NodeI, data int) *piscine.NodeI {
+	n := &piscine.NodeI{Data: data}
+
+	if l == nil {
+		return n
+	}
+	iterator := l
+	for iterator.Next != nil {
+		iterator = iterator.Next
+	}
+	iterator.Next = n
+	return l
+}
+
+func main() {
+	var link *piscine.NodeI
+	var link2 *piscine.NodeI
+
+	link = listPushBack(link, 3)
+	link = listPushBack(link, 5)
+	link = listPushBack(link, 7)
+
+	link2 = listPushBack(link2, -2)
+	link2 = listPushBack(link2, 9)
+
+	PrintList(piscine.SortedListMerge(link2, link))
+}
