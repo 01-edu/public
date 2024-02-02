988d47fa7e45fae9ad0051740b5d3203bd2e802a
Harry <14015057+HarryVasanth@users.noreply.github.com>
2024-02-02 11:51:02 +0000

    fix(configure): install method `docker` & `git`

--- sh/debian/configure.sh
+++ sh/debian/configure.sh
@@ -138,23 +138,13 @@ EOF
   sed -i '/swap/d' /etc/fstab
 
   # Docker
-  apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-  curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-  apt-get update
-  apt-get -y install docker-ce docker-ce-cli containerd.io
-
-  # Docker compose
-  curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
-  curl -L https://raw.githubusercontent.com/docker/compose/1.29.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+  curl -fsSL https://get.docker.com -o get-docker.sh | bash
 
   # NodeJS
   curl -fsSL https://deb.nodesource.com/setup_20.x | bash && apt-get install -y nodejs
 
   # Git
-  echo deb http://deb.debian.org/debian buster-backports main | tee /etc/apt/sources.list.d/buster-backports.list
-  apt-get update && apt-get -y install -t buster-backports git
+  apt-get update && apt-get -y install git
 
   # Generate SSH key
   ssh-keygen -ted25519 -f ~/.ssh/id_ed25519 -N ''
