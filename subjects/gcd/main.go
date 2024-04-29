529e5c8fdbad5f966300ab7618385d3ffa829112
nprimo <primo.niccolo@gmail.com>
2024-04-29 12:10:41 +0100

    docs(gcd): update subject to require a function instead of a program
    - add main.go to test the function in exam mode

---
+++ subjects/gcd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Gcd(42, 10))
+	fmt.Println(piscine.Gcd(42, 12))
+	fmt.Println(piscine.Gcd(14, 77))
+	fmt.Println(piscine.Gcd(17, 3))
+}
