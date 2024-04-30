6558757d7790b395492ccd32e3c65895a91c8b51
nprimo <primo.niccolo@gmail.com>
2024-04-30 14:42:08 +0100

    chore(stock-market): run formatter

--- subjects/mobile-dev/stock-market/resources/mock-stock-data-server/app.py
+++ subjects/mobile-dev/stock-market/resources/mock-stock-data-server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 import time
-from utils import load_historical_data 
+from utils import load_historical_data
 
 app = Flask(__name__)
 CORS(app)
@@ -11,20 +11,23 @@ start_time = time.time()
 
 historical_data = load_historical_data()
 
+
 @app.route('/exchange_rate/<symbol>')
 def get_stock_data(symbol):
     if symbol not in list(historical_data.keys()):
         return jsonify("Invalid symbol")
     current_time = time.time()
-    step = (int(current_time * 10) - int(start_time * 10)) % len(historical_data[symbol])
+    step = (int(current_time * 10) - int(start_time * 10)
+            ) % len(historical_data[symbol])
     try:
         return jsonify({
             'symbol': 'USD',
             'rate': float(historical_data[symbol][step]),
             'timestamp': current_time
-            })
+        })
     except:
         return "Server Error"
+
 
 @app.route('/stocks_list')
 def list_symbols():
