f5aa56821acf89fd6c71b0517064ea48b9aa3830
nprimo <primo.niccolo@gmail.com>
2024-06-03 12:15:48 +0100

    feat(repeatalpha): update subject to ask for a function instead of a program
    - add main.go for exam

---
+++ subjects/repeatalpha/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(RepeatAlpha("abc"))
+	fmt.Println(RepeatAlpha("Choumi."))
+	fmt.Println(RepeatAlpha(""))
+	fmt.Println(RepeatAlpha("abacadaba 01!"))
+}
