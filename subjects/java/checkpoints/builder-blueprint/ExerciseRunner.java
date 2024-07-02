868e5821e684b08001c36987029a1600edeaaa12
amin <amin@MacBook-Pro-de-amin.local>
2024-07-02 15:24:22 +0100

    docs: adding subject and main

---
+++ subjects/java/checkpoints/builder-blueprint/ExerciseRunner.java
@@ -0,0 +1,11 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        RegexDirector director = new RegexDirector();
+        RegexBuilder builder = new ConcreteRegexBuilder();
+        
+        director.setBuilder(builder);
+        Regex regex = director.construct();
+        
+        System.out.println(regex);
+    }
+}
