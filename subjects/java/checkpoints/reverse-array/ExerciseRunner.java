f9d5895fe80d49fdd8dd569d7f868885c614d0a9
amin <amin@MacBook-Pro-de-amin.local>
2024-07-03 14:50:29 +0100

    docs: adding messing imports to the main files

--- subjects/java/checkpoints/reverse-array/ExerciseRunner.java
+++ subjects/java/checkpoints/reverse-array/ExerciseRunner.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.Arrays;
+
 public class ExerciseRunner {
     public static void main(String[] args) throws IOException {
         System.out.println(Arrays.toString(ReverseArray.reverse(new Integer[]{4, 2, 1, 3})));
--- subjects/java/checkpoints/rotate-array/ExerciseRunner.java
+++ subjects/java/checkpoints/rotate-array/ExerciseRunner.java
@@ -1,5 +1,2 @@
-public class ExerciseRunner {
-    public static void main(String[] args) throws IOException {
-        System.out.println(Arrays.toString(RotateArray.rotate(new Integer[]{4, 2, 1, 3}, 1)));
-    }
-}
+import java.io.*;
+import java.util.Arrays;
