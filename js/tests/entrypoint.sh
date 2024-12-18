4c067366d3edee68cbe8ae35e492cb2dc454d6f8
zanninso <amine_19961@hotmail.com>
2024-12-18 20:55:23 +0100

    tests: adding option to prevent students from using eval for elementary exercise

--- js/tests/entrypoint.sh
+++ js/tests/entrypoint.sh
@@ -7,4 +7,9 @@ if test "$CODE_EDITOR_RUN_ONLY" = true; then
 	exit
 fi
 
+if test $EXERCISE = "elementary"; then
+	node  --disallow-code-generation-from-strings /app/test.mjs "/jail/student" "${EXERCISE}" 
+	exit
+fi
+
 node /app/test.mjs "/jail/student" "${EXERCISE}"
