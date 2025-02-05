926aaee2b835c22617a0ba54ec0027d80dba2c89
Harry <14015057+HarryVasanth@users.noreply.github.com>
2025-02-05 17:45:18 +0000

    fix(config): keep ufw ipv6

--- sh/debian/configure.sh
+++ sh/debian/configure.sh
@@ -106,12 +106,6 @@ EOF
     ufw allow in 8080/tcp
     ufw allow in 8082/tcp
     ufw logging off
-    ufw --force enable
-    ufw --force delete 6
-    ufw --force delete 6
-    ufw --force delete 6
-    ufw --force delete 6
-    ufw --force delete 6
 
     # Optimize
     systemctl disable apt-daily.timer apt-daily-upgrade.timer remote-fs.target man-db.timer
